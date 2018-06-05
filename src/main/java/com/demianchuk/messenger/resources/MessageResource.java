ee5407e5563c57bf00f5fb8fcc1efebc77e5eb49
Nick <nicksetone@gmail.com>
2018-06-05 10:49:54 -0500

    Implement GET requests

--- src/main/java/com/demianchuk/messenger/resources/MessageResource.java
+++ src/main/java/com/demianchuk/messenger/resources/MessageResource.java
@@ -3,9 +3,7 @@ package com.demianchuk.messenger.resources;
 import com.demianchuk.messenger.models.Message;
 import com.demianchuk.messenger.service.MessageService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -15,8 +13,34 @@ public class MessageResource {
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
     }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message getMessage(@PathParam("messageId") long messageId) {
+        return messageService.getMessage(messageId);
+    }
+
+//    @POST
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Message addMessage() {
+//        return messageService.addMessage(new Message(3L, "Hello Message", "Kolya"));
+//    }
+//
+//    @PUT
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Message updateMessage() {
+//        return messageService.updateMessage(new Message(1L, "HELLO WORLD!!!", "NICK"));
+//    }
+//
+//    @DELETE
+//    @Path("/{messageId}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Message removeMessage(@PathParam("messageId") long messageId) {
+//        return messageService.removeMessage(messageId);
+//    }
 }
